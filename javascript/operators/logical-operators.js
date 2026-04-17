4d4097e070a66df4d56c29ec6cb54f1031d7f840
Gerfesson Abel <abel.am.pro@gmail.com>
2026-04-16 20:30:36 -0400

    docs: add logical operators study examples

--- javascript/operators/logical-operators.js
+++ javascript/operators/logical-operators.js
@@ -1,5 +1,34 @@
 /*
-&& -> E -> AND
-|| -> OU -> OR
-! -> NÃO -> NOT
+&& -> E -> AND -> todas as expressões devem ser verdadeiras para retornar true 
+|| -> OU -> OR -> pelo menos uma expressão deve ser verdadeira 
+! -> NÃO -> NOT -> inverte uma expressão
 */
+
+// expressão AND
+const expressaoAnd1 = true && true && true;
+console.log(expressaoAnd1); // retorna true
+
+const expressaoAnd2 = true && false && true;
+console.log(expressaoAnd2); // retorna false
+
+// expressão OR
+const expressaoOr3 = true || true;
+console.log(expressaoOr3); // retorna true
+
+const expressaoOr4 = true || false 
+console.log(expressaoOr4) // retorna true 
+
+// expressão not
+console.log(!true) // false
+console.log(!false) // true
+console.log(!!true); // true
+console.log(!!false) // false
+
+
+// exercicio 
+const username = 'gabelcodes';
+const userPassword = '12345';
+
+const login = username=== 'gabelcodes' && userPassword === '12345' ? 'Acesso permitido' : 'Usuário ou senha inválidos'
+console.log(login)
+
