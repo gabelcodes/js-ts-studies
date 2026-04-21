ae8890982ba9a909a1eedebb6938c1d59e3e5a83
Gerfesson Abel <abel.am.pro@gmail.com>
2026-04-21 16:19:19 -0400

    docs: add date objects lesson with practical examples

---
+++ javascript/objects/date-object.js
@@ -0,0 +1,19 @@
+function zeroEsquerda(num) {
+    return num = num >= 10 ? num : `0${num}`
+}
+
+function formataData(data) {
+    const dia = zeroEsquerda(data.getDate());
+    const mes = zeroEsquerda(data.getMonth() + 1);
+    const ano = zeroEsquerda(data.getFullYear());
+    const hora = zeroEsquerda(data.getHours());
+    const min = zeroEsquerda(data.getMinutes());
+    const seg = zeroEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil)
